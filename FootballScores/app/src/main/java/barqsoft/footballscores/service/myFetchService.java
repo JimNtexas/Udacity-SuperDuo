19e6c8d48a8446d0653f5179036bb4c38c9db24a
JimNtexas <grayraven42@gmail.com>
2015-10-06 20:57:30 -0500

    increased text size of day pager tab to enhance app readablity

--- FootballScores/app/src/main/java/barqsoft/footballscores/service/myFetchService.java
+++ FootballScores/app/src/main/java/barqsoft/footballscores/service/myFetchService.java
@@ -52,7 +52,7 @@ public class myFetchService extends IntentService
 
         Uri fetch_build = Uri.parse(BASE_URL).buildUpon().
                 appendQueryParameter(QUERY_TIME_FRAME, timeFrame).build();
-        Log.v(LOG_TAG, "The url we are looking at is: "+fetch_build.toString()); //log spam
+       // Log.v(LOG_TAG, "The url we are looking at is: "+fetch_build.toString()); //log spam
         HttpURLConnection m_connection = null;
         BufferedReader reader = null;
         String JSON_data = null;
@@ -64,7 +64,7 @@ public class myFetchService extends IntentService
             m_connection.addRequestProperty("X-Auth-Token",getString(R.string.api_key));
             m_connection.connect();
 
-            Log.d(LOG_TAG, "Api key:" + getString(R.string.api_key));
+         //   Log.d(LOG_TAG, "Api key:" + getString(R.string.api_key));
 
             // Read the input stream into a String
             InputStream inputStream = m_connection.getInputStream();
